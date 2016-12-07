4060df6171a1317cc6a5fd3cdb4c6a5cc96c4a33
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-07 11:23:38 -0200

    Adds babel to transpile backend code.

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,8 @@
-var serve = require('koa-static');
-var koa = require('koa');
-var app = koa();
+import serve from 'koa-static';
+import koa from 'koa';
 
-app.use(serve(__dirname + '/../client'));
+const CLIENT_FILES = __dirname + '/../client';
 
+const app = koa();
+app.use(serve(CLIENT_FILES));
 app.listen(3000);
