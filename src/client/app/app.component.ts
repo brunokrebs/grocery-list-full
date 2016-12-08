456c1a917333175536c7c95b859b5d7c79779249
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-08 18:12:26 -0200

    Adds livereloading to the boilerplate

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = `I'm here!`;
+    title = `Look! I have livereloading now!!`;
 }
