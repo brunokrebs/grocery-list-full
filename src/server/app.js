5feb2a0e673f662e0512c17fcd559b103d66c13f
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-08 11:32:16 -0200

    Adds webpack, angular dependencies and a bunch of tools to transpile typescript and bundle an angular app.

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 import serve from 'koa-static';
 import koa from 'koa';
 
-const CLIENT_FILES = __dirname + '/../client';
+const CLIENT_FILES = './dist';
 
 const app = koa();
 app.use(serve(CLIENT_FILES));
