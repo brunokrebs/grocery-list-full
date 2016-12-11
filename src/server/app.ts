a7daccc7cec4c43bf8233f2c81bba97542a6a489
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-11 18:43:21 -0200

    Adds LokiJS to persist data.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,31 @@
 import * as Koa from 'koa';
 import * as StaticFiles from 'koa-static';
 import * as BodyParser from 'koa-bodyparser';
+import * as Lokijs from 'lokijs';
 import * as Router from 'koa-router';
 import { Error } from '../common/error';
 
 const CLIENT_FILES = './dev/client/';
+
+const DB = new Lokijs('klan-database.json', {
+    autosave: true
+});
+const USER_DB = DB.addCollection('user');
+
+let admin = USER_DB.findOne({
+    email: 'me@brunokrebs.com'
+});
+if (!admin) {
+    console.log('inserting base user');
+    USER_DB.insert({
+        email:'me@brunokrebs.com',
+        password: 'password',
+        admin: 'true'
+    });
+    USER_DB.commit();
+}
+
+admin = USER_DB.findOne({'email':'me@brunokrebs.com'});
 
 // FIXME refactor it to something like a Server class
 const SERVER = new Koa();
@@ -17,16 +38,18 @@ SERVER.use(ROUTER.routes());
 
 // routes
 ROUTER.post('/api/authenticate', function *() {
-    if (this.request.body.email == 'me@brunokrebs.com') {
-        this.body = {
-            email: `me@brunokrebs.com`
-        };
+    let user = USER_DB.findOne({
+        $and: [
+            { email: this.request.body.email },
+            { password: this.request.body.password }
+        ]
+    });
+    if (user) {
+        this.body = user;
     } else {
         let error = new Error(401, `Who are you?`);
         this.status = error.statusCode;
-        let err = error.toObject();
-        console.log(err);
-        this.body = err;
+        this.body = error.toObject();
     }
 });
 
