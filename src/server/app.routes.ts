14991a4ecbae9935ea4e2a919d9925f756aa3a90
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-12 10:45:05 -0200

    Lets Angular SPA handle all non api requests.

--- src/server/app.routes.ts
+++ src/server/app.routes.ts
@@ -1,7 +1,25 @@
 import * as Router from "koa-router";
+import * as fs from "fs";
 import AUTHENTICATION_ROUTES from "./authentication/authentication.routes";
 
 const ROUTER = new Router();
+
+const LOAD_HTML = function() {
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./dev/client/index.html', {'encoding': 'utf8'}, function (err, data) {
+            if(err) return reject(err);
+            resolve(data);
+        });
+    });
+};
+
+ROUTER.get(/^\/(.*)(?:\/|$)/, function *(next) {
+    if (this.request.url.startsWith("/api")) {
+        yield next;
+    } else {
+        this.body = yield LOAD_HTML();
+    }
+});
 
 AUTHENTICATION_ROUTES.posts
     .forEach(route => ROUTER.post(route.path, route.middleware));
