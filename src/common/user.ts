c2299fc96520a18d8cc6c59bb6d929a6ca68133a
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-12 10:42:37 -0200

    Adds a new parameter to User.

--- src/common/user.ts
+++ src/common/user.ts
@@ -4,10 +4,12 @@ export class User {
     @autoserialize public email: String;
     @autoserialize public password: String;
     @autoserialize public admin: boolean;
+    @autoserialize public name: String;
 
-    constructor(email: String, password: String, admin?: boolean) {
+    constructor(email: String, password: String, name?: String, admin?: boolean) {
         this.email = email;
         this.password = password;
         this.admin = admin;
+        this.name = name;
     }
 }
