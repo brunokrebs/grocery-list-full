6bc77f821075c7ff70883cc3417e91f01af88806
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-12 10:45:38 -0200

    Adds another route to SPA

--- src/client/app/app.routing.ts
+++ src/client/app/app.routing.ts
@@ -1,9 +1,11 @@
 import {RouterModule} from "@angular/router";
 
 import {SignInComponent} from "./sign-in/sign-in.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 const APP_ROUTES = [
-    { path: '', component: SignInComponent }
+    { path: '', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];
 
 export const Routing = RouterModule.forRoot(APP_ROUTES);
