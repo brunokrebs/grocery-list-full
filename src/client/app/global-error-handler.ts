21bb1b76b4890dc64bb05991e94040379d4e33be
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-12 10:43:07 -0200

    Fixes global error handler.

--- src/client/app/global-error-handler.ts
+++ src/client/app/global-error-handler.ts
@@ -4,7 +4,7 @@ import { Error } from '../../common/error';
 
 export class GlobalErrorHandler implements ErrorHandler {
     handleError(error : any) {
-        if (error.rejection) {
+        if (error.rejection && typeof error.rejection.json == 'function') {
             let myErrorObj: Error = error.rejection.json();
             alert(myErrorObj.statusCode + ': ' + myErrorObj.message);
         } else {
