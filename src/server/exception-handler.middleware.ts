433a2385c898297304df6a64f6fe6ae47b699169
Bruno Krebs <krebs.bruno@gmail.com>
2017-01-04 14:26:48 -0200

    Logging error catch on server.

--- src/server/exception-handler.middleware.ts
+++ src/server/exception-handler.middleware.ts
@@ -4,6 +4,7 @@ export default function *(next) {
     try {
         yield next;
     } catch (err) {
+        console.log(err);
         if (err instanceof Exception) {
             // it transform the exception to an object literal
             this.body = err.toObject();
