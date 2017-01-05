69369e5e48a017581ae87ea1d5701669577ef9ba
Bruno Krebs <krebs.bruno@gmail.com>
2017-01-05 12:49:25 -0200

    Token is not needed.

--- src/common/user.ts
+++ src/common/user.ts
@@ -1,10 +1,8 @@
 export class User {
     public email: string;
-    public token?: string;
     public items: Array<string>;
 
     public static OnSerialized(instance : User, json : any) : void {
-        delete json.password;
         delete json.meta;
     }
 }
